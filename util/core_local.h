bfbe86fb1a47b42dd146378ad60a95c6666ebe0c
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-27 10:21:12 +0400

    tmp

--- util/core_local.h
+++ util/core_local.h
@@ -43,12 +43,12 @@ class CoreLocalArray {
 
 template <typename T>
 CoreLocalArray<T>::CoreLocalArray() {
-  int num_cpus;
-  if (const char* max_cpus = std::getenv("ROCKSDB_MAX_CPUS")) {
-    num_cpus = atoi(max_cpus);
-  } else {
-    num_cpus = static_cast<int>(std::thread::hardware_concurrency());
-  }
+  int num_cpus = 2;
+  // if (const char* max_cpus = std::getenv("ROCKSDB_MAX_CPUS")) {
+  //   num_cpus = atoi(max_cpus);
+  // } else {
+  //   num_cpus = static_cast<int>(std::thread::hardware_concurrency());
+  // }
   // find a power of two >= num_cpus and >= 8
   size_shift_ = 3;
   while (1 << size_shift_ < num_cpus) {
